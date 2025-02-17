7de3b3964aac67dcc0fee2051151db9f487f6f04
Gabriel Alves Varella da Costa <gabriel.varella1327@hotmail.com>
2025-02-17 17:07:50 -0300

    feat: Implementando o método de atualizar o produto na aplicação e de listar os pordutos.

--- app.py
+++ app.py
@@ -8,12 +8,15 @@ app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///ecommerce.db'
 # Modelagem
 db = SQLAlchemy(app)
 
+
+
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
     price = db.Column(db.Float, nullable=False)
     description = db.Column(db.Text, nullable=True)
 
+# Adicionando porduto
 @app.route('/api/products/add', methods=["POST"])
 def add_products():
     data = request.json
@@ -24,6 +27,7 @@ def add_products():
         return jsonify({"message":"Product added successfully"})
     return jsonify({"message":"Invalid product data"}), 400
 
+#Deletando produro
 @app.route('/api/products/delete/<int:product_id>', methods=["DELETE"])
 def delete_product(product_id):
     #Recuperar o produto da base de dados
@@ -37,6 +41,7 @@ def delete_product(product_id):
         return jsonify({"message":"Product deleted successfully"})
     return jsonify({"message":"Product not found"}), 404
 
+# Buscanndo o porduto
 @app.route('/api/products/<int:product_id>', methods=["GET"])
 def get_product_details(product_id):
     product=Product.query.get(product_id)
@@ -49,8 +54,46 @@ def get_product_details(product_id):
         })
     return jsonify({"message":"Product not found"}), 400
 
-#Definir uma rota raiz (página inicial) e a função que será executada ao requisitar 
+# Atualiza produto
+@app.route('/api/products/update/<int:product_id>', methods=["PUT"])
+def upade_product(product_id):
 
+    product= Product.query.get(product_id)
+    
+    if not product:
+        return jsonify({"message":"Product not found"}), 404
+    
+    data=request.json
+    if "name" in  data:
+        product.name=data['name']
+
+    if "price" in  data:
+        product.price=data['price']
+
+    if "description" in  data:
+        product.description=data['description']
+
+    db.session.commit()
+
+    return  jsonify({"message":"Product updated successfully"})
+
+
+
+#Definir uma rota raiz (página inicial) e a função que será executada ao requisitar 
+@app.route('/api/products', methods=["GET"])
+def get_products():
+    products=Product.query.all()
+    # if products:
+    products_list=[]
+    for product in products:
+        product_data={
+                "id" :product.id,
+                "name" :product.name,
+                "price" :product.price
+            }
+        products_list.append(product_data)
+    return jsonify(products_list)
+    # return jsonify({"message":"Product not found"}), 400
 
 
 # @app.route('/')
