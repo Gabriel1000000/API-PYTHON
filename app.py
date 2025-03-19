ec0b5494cb467296ac759c42cb7ce4b26ceb5288
Gabriel Alves Varella da Costa <gabriel.varella1327@hotmail.com>
2025-03-19 13:09:12 -0300

    feat: Implementando a requisições de carrinho.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 
 app=Flask(__name__)
 app.config['SECRET_KEY']='minha_chave'
@@ -19,8 +19,9 @@ CORS(app)
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    user = db.Column(db.String(80), nullable=False, unique=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
+    cart=db.relationship('CartIntem', backref='user', lazy=True)
 
 
 class Product(db.Model):
@@ -28,6 +29,14 @@ class Product(db.Model):
     name = db.Column(db.String(120), nullable=False)
     price = db.Column(db.Float, nullable=False)
     description = db.Column(db.Text, nullable=True)
+
+
+class CartIntem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(80), db.ForeignKey('user.id'), nullable=False)
+    product_id = db.Column(db.String(80), db.ForeignKey('product.id'), nullable=False)
+    
+    
 
 # Rota de autenticação configurado para o login_required, essa sunção carrega o usuario autenticado pora o @login_required
 @login_manager.user_loader
@@ -39,7 +48,7 @@ def load_user(user_id):
 def login():
     data=request.json
     
-    user=User.query.filter_by(user=data.get("user")).first()
+    user = User.query.filter_by(username=data.get("username")).first()
     
     if user and data.get("password") == user.password:
             login_user(user)
@@ -115,8 +124,6 @@ def upade_product(product_id):
 
     return  jsonify({"message":"Product updated successfully"})
 
-
-
 # Apresentar todos os produtos
 @app.route('/api/products', methods=["GET"])
 def get_products():
@@ -133,10 +140,57 @@ def get_products():
     return jsonify(products_list)
     # return jsonify({"message":"Product not found"}), 400
 
+@app.route('/api/cart/add/<int:product_id>', methods=["POST"])
+@login_required
+def add_to_cart(product_id):
+    # User
+    user=User.query.get(int(current_user.id))
+    # Porduct
+    product=Product.query.get(product_id)
 
-# @app.route('/')
-# def hello_wolrd():
-#     return "Hello wolrd!!"
+    if user and product:
+        cart_item= CartIntem(user_id=user.id, product_id=product.id)
+        db.session.add(cart_item)
+        db.session.commit()
+        return jsonify({'message': 'Item added to the car sucessfully'})
+    return jsonify({'message': 'Failed to add item to the cart'}), 400
+
+@app.route('/api/cart/remove/<int:product_id>', methods=["DELETE"])
+@login_required
+def remove_from_car(product_id):
+    cart_item=CartIntem.query.filter_by(user_id=current_user.id,product_id=product_id).first()
+    if cart_item:
+        db.session.delete(cart_item)
+        db.session.commit()
+        return jsonify({'message':'Item removed from the cart successfully'})
+    return jsonify({'message':'Failed to remove item from the cart'}), 400
+
+@app.route('/api/cart', methods=["GET"])
+@login_required
+def view_cat():
+    user= User.query.get(int(current_user.id))
+    cart_items=user.cart
+    cart_content=[]
+    for cart_item in cart_items:
+        product=Product.query.get(cart_item.product_id)
+        cart_content.append({
+            'id': cart_item.id,
+            'user_id':cart_item.user_id,
+            'product_id': cart_item.product_id,
+            'product_name': product.name,
+            'product_price': product.price
+        })
+    return jsonify(cart_content)
+
+@app.route('/api/cart/checkout', methods=["POST"])
+@login_required
+def checkout():
+    user=User.query.get(int(current_user.id))
+    cart_items=user.cart
+    for cart_item in cart_items:
+        db.session.delete(cart_item)
+    db.session.commit()
+    return jsonify({'message':'Checkout successful. Cart has been cleared.'})
 
 if __name__=="__main__":
     app.run(debug=True)
